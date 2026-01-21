90d3c786bf0a18b391dffe93fc07bbbb4c59498f
cuuse3 <cuuse3@gmail.com>
2026-01-21 05:37:08 +0000

    h

--- unit1/crudvector.cpp
+++ unit1/crudvector.cpp
@@ -5,18 +5,22 @@ using namespace std;
 
 int main()
 {
-    int b;
+    int b,s;
     vector<int> a ;
+    cout << "Enter the Size of vector initially: " ;
+    cin >> s;
     cout << "Enter 5 elements for the first vector:\n";
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < s; i++)
     {
         cin >> b;
         a.push_back(b);
     }
- 
+    
+    
     a.push_back(120); 
     a.push_back(130); 
     a.pop_back();
+
 
     cout << "Vector elements are: ";
     for(size_t i = 0; i < a.size() ; i++)
